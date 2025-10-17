2c443c98cef19b639c6d15ecc836942e25ee6f12
Your Name <you@example.com>
2025-10-17 22:17:59 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Отдаём манифест
+@app.route('/manifest.json')
+def manifest():
+    return send_from_directory('.', 'manifest.json')
+
+# Отдаём иконки
+@app.route('/static/icons/<path:filename>')
+def icons(filename):
+    return send_from_directory('static/icons', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
